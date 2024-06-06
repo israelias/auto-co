7bb021ccba73f300331e629b6e200753b03842d5
Joem Elias Sanez <israelias.js@gmail.com>
2024-06-06 16:22:45 +0000

    feat: commit copilot 2

---
+++ comments.js
@@ -0,0 +1,35 @@
+// Create web server
+import http from 'http';
+import fs from 'fs';
+import url from 'url';
+
+// Create a server
+http.createServer(function (req, res) {
+  // Parse the request containing file name
+  var pathname = url.parse(req.url).pathname;
+  // Print the name of the file for which request is made.
+  console.log("Request for " + pathname + " received.");
+
+  // Read the requested file content from file system
+  fs.readFile(pathname.slice(1), function (err, data) {
+    if (err) {
+      console.log(err);
+      // HTTP Status: 404 : NOT FOUND
+      // Content Type: text/plain
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+    } else {
+      // Page found
+      // HTTP Status: 200 : OK
+      // Content Type: text/plain
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+
+      // Write the content of the file to response body
+      res.write(data.toString());
+    }
+    // Send the response body 
+    res.end();
+  });
+}).listen(8080);
+
+// Console will print the message
+console.log('Server running at http://localhost:8080');
